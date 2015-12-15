256f1fb2b6e83e0369e639467d72d5c9d29597b7
Tigers <ericfallone@gmail.com>
2015-12-15 15:29:01 -0500

    Inactive Cube Movement
    Nodes that are unselected now move around.

--- UnityFolder/Assets/Scripts/Cube_Node.cs
+++ UnityFolder/Assets/Scripts/Cube_Node.cs
@@ -12,7 +12,12 @@ public class Cube_Node : MonoBehaviour {
 
 	float lerpSmoothness = .025f; 
 
+	float rotSpeed = 2;
+	
+	float maxDistFromCenter =.1f; 
 
+	bool isMoving = true;
+	//float maxDistMove =.05f;
 
 	void Start(){
 		playNum = 2;
@@ -21,13 +26,21 @@ public class Cube_Node : MonoBehaviour {
 		}
 	}
 
+	void Update(){
+		if(model == null){
+			return;
+		}
+		model.transform.Rotate (Vector3.right * rotSpeed);
+	}
+
+
 	public void claimNode(int curPlayer){
 		playNum = curPlayer;
 		StartCoroutine (lerpColor (GameRunner.S.playerMat [curPlayer].color));
 		StartCoroutine (lerpScale ( GameRunner.S.choiceSize , 3f));
 	}
 	public void scaleNode(float start, float end){
-		StopAllCoroutines ();
+		StopCoroutine ("lerpScale");
 		StartCoroutine (lerpScale (start,end));
 	}
 	public void changeColorNode(Color end){
@@ -42,6 +55,7 @@ public class Cube_Node : MonoBehaviour {
 	}
 
 	IEnumerator lerpScale(float start, float end){
+		isMoving = (end < start);
 		float cur;
 		for(float c =0 ; c<1.05;c=c+.1f){
 			cur = Mathf.Lerp(start,end,c);
@@ -52,6 +66,9 @@ public class Cube_Node : MonoBehaviour {
 
 	public Vector3 endPos; 
 	IEnumerator startingMoveMent(){
+		if(model == null){
+			yield break;
+		}
 		/*Vector3*/ endPos = this.transform.position;
 		Vector3 startPos = new Vector3 (0,0,0);
 		for(float i =0 ; i <=1 ; i = i + 1/incrementsToTarget){
@@ -59,5 +76,37 @@ public class Cube_Node : MonoBehaviour {
 			yield return new WaitForSeconds(smoothness);
 		}
 		this.transform.position = endPos;
+		StartCoroutine (restingMovement());
+	}
+
+	public Vector3 target;
+
+	IEnumerator restingMovement(){
+		while (true) {
+
+			while(Cube_Movement.S.state == State.moving){
+				yield return new WaitForSeconds(lerpSmoothness);
+			}
+			
+			target = new Vector3 (
+				Random.Range (-maxDistFromCenter, maxDistFromCenter),
+				Random.Range (-maxDistFromCenter, maxDistFromCenter),
+				Random.Range (-maxDistFromCenter, maxDistFromCenter)
+			);
+			if(isMoving== false){
+				target= Vector3.zero;
+			}
+			target = target + this.transform.position;
+
+			for(float c =0 ; c<1.05;c=c+.1f){
+				Vector3 startPos = model.transform.position;
+				Vector3 endPosW =target;
+				model.transform.position = Vector3.Lerp(startPos,endPosW,c);
+				yield return new WaitForSeconds(lerpSmoothness);
+				if(Cube_Movement.S.state == State.moving){
+					c=2;
+				}
+			}
+		}
 	}
 }
